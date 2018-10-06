0ff295deb912b94fd5dd7cdbc24504a77aff894e
tarman1808 <tarman1808@gmail.com>
2018-10-06 09:39:17 +0700

    coding main java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,4 +1,55 @@
 package application;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.application.Application.launch;
+
 public class Main {
+
+    public static Stage stage;
+    private static BorderPane mainlayout;
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Main.stage = primaryStage;
+        Main.stage.setTitle("Karawang");
+        stage.setMaximized(true);
+        Main.showMain();
+        Main.showLogin();
+
+    }
+
+    public static void showMain() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("view/MainView.fxml"));
+        mainlayout = loader.load();
+        Scene scene = new Scene(mainlayout);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void showLogin() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("view/Login.fxml"));
+        BorderPane mainItem = loader.load();
+        mainlayout.setCenter(mainItem);
+
+    }
+
+    public static void showHome() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("view/Home.fxml"));
+        BorderPane mainItem = loader.load();
+        mainlayout.setCenter(mainItem);
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
