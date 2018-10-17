b837d75f4a4d9b25693313b12e501b83cd63035b
tarman1808 <tarman1808@gmail.com>
2018-10-17 16:25:58 +0700

    init

--- src/application/Main.java
+++ src/application/Main.java
@@ -51,7 +51,7 @@ public class Main extends Application {
 
     public static void showUser() throws IOException {
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("view/Home.fxml"));
+        loader.setLocation(Main.class.getResource("view/User.fxml"));
         AnchorPane mainItem = loader.load();
         mainlayout.setCenter(mainItem);
 
